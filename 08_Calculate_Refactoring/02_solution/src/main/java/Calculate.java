a98c461c8c44d5553d0e81489d0b265b0235097e
Cédric PONTET <cpontet@agilepartner.net>
2023-07-13 10:34:56 +0200

    used smart enum with strangler fig pattern

--- 08_Calculate_Refactoring/02_solution/src/main/java/Calculate.java
+++ 08_Calculate_Refactoring/02_solution/src/main/java/Calculate.java
@@ -1,58 +1,19 @@
 // Exercise: refactor me
 public class Calculate 
 {
-
-    public static final double SIMPLE_CUSTOMER_RATIO = 0.1;
-    public static final double VALUABLE_CUSTOMER_RATIO = 0.3;
-    public static final double MOST_VALUABLE_CUSTOMER_RATIO = 0.5;
-
-    /**
-     * Calculate the price of a given item for a specified customer
-     * @param amount price
-     * @param type Account status (NotRegistered = 1, SimpleCustomer = 2, ValuableCustomer = 3, MostValuableCustomer = 4)
-     * @param years Time in years that the client is our customer
-     * @return The price
-     */
     public static double calculate(double amount, int type, int years)
-    {  
-        double result = 0;  
-        double disc = getDiscount(years);
-
-        double amountMinusRatio = amount;
-
-        // NotRegistered  
-        if (type == 1)  
-        {  
-                result = amount;
-                return result;
-        }  
-        // SimpleCustomer  
-        else if (type == 2)  
-        {
-            amountMinusRatio = amountMinusRatio(amount, SIMPLE_CUSTOMER_RATIO);
-        }
-        // ValuableCustomer  
-        else if (type == 3)  
-        {
-            amountMinusRatio = amountMinusRatio(amount, VALUABLE_CUSTOMER_RATIO);
-        }
-        // MostValuableCustomer  
-        else if (type == 4)  
-        {
-            amountMinusRatio = amountMinusRatio(amount, MOST_VALUABLE_CUSTOMER_RATIO);
-        }
-
-        result = amountMinusRatio - disc * amountMinusRatio;
-
-        return result;  
+    {
+        CustomerType customerType = CustomerType.values()[type - 1];
+        return  calculate(amount, customerType, years);
     }
 
-    private static double getDiscount(int years) {
-        return (years > 5) ? (double) 5 / 100 : (double) years / 100;
+    public static double calculate(double amount, CustomerType type, int years)
+    {
+        double amountMinusRatio = amountMinusRatio(amount, type.ratio());
+        return amountMinusRatio - type.discount(years) * amountMinusRatio;
     }
 
     private static double amountMinusRatio(double amount, double ratio) {
         return  amount - (amount * ratio);
     }
-
 }
