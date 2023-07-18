e7efc18400869ae83406432bcd0f40283e5bb689
Cédric PONTET <cpontet@agilepartner.net>
2023-07-18 15:37:57 +0200

    improved fizzbuzz

--- 01_FizzBuzz/java/02_solution/src/test/java/fizzbuzz_should.java
+++ 01_FizzBuzz/java/02_solution/src/test/java/fizzbuzz_should.java
@@ -1,7 +1,8 @@
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Assertions;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class fizzbuzz_should {
 
@@ -17,27 +18,27 @@ class fizzbuzz_should {
     public void return_the_number(Integer value) {
         String expected = value.toString();
         String actual = fizzbuzz.get(value);
-        Assertions.assertEquals(expected, actual);
+        assertThat(actual).isEqualTo(expected);
     }
 
     @ParameterizedTest
     @ValueSource(ints = {3, 6, 9, 12, 18, 21, 24, 27, 33, 36, 39, 42, 48, 51, 54, 57, 63, 66, 69, 72, 78, 81, 84, 87, 93, 96, 99})
     public void return_fizz_for_multiples_of_3(Integer value) {
         String actual = fizzbuzz.get(value);
-        Assertions.assertEquals(FizzBuzz.FIZZ, actual);
+        assertThat(actual).isEqualTo("Fizz");
     }
 
     @ParameterizedTest
     @ValueSource(ints = {5, 10, 20, 25, 35, 40, 50, 55, 65, 70, 80, 85, 95, 100})
     public void return_buzz_for_multiples_of_5(Integer value) {
         String actual = fizzbuzz.get(value);
-        Assertions.assertEquals(FizzBuzz.BUZZ, actual);
+        assertThat(actual).isEqualTo("Buzz");
     }
 
     @ParameterizedTest
     @ValueSource(ints = {15, 30, 45, 60, 75, 90})
     public void return_fizzbuzz_for_multiples_of_3_and_5(Integer value) {
         String actual = fizzbuzz.get(value);
-        Assertions.assertEquals(FizzBuzz.FIZZBUZZ, actual);
+        assertThat(actual).isEqualTo("FizzBuzz");
     }
 }
