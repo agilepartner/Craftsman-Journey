251195adaf8ab9b3a02742d34de63275b6459f0b
Cédric PONTET <cpontet@agilepartner.net>
2023-07-24 14:31:58 +0200

    verify in order

--- 05_Bank_OutsideIn/java/01_start/src/test/java/bank/acceptance/PrintStatementFeature.java
+++ 05_Bank_OutsideIn/java/01_start/src/test/java/bank/acceptance/PrintStatementFeature.java
@@ -2,6 +2,7 @@ package bank.acceptance;
 
 import bank.*;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.junit.jupiter.api.Test;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -22,10 +23,11 @@ public class PrintStatementFeature {
 
         account.printStatement();
 
-        verify(console).printLine("DATE | AMOUNT | BALANCE");
-        verify(console).printLine("10/04/2014 | 500.00 | 1400.00");
-        verify(console).printLine("02/04/2014 | -100.00 | 900.00");
-        verify(console).printLine("01/04/2014 | 1000.00 | 1000.00");
+        InOrder inOrder = inOrder(console);
+        inOrder.verify(console).printLine("DATE | AMOUNT | BALANCE");
+        inOrder.verify(console).printLine("10/04/2014 | 500.00 | 1400.00");
+        inOrder.verify(console).printLine("02/04/2014 | -100.00 | 900.00");
+        inOrder.verify(console).printLine("01/04/2014 | 1000.00 | 1000.00");
     }
 
 }
