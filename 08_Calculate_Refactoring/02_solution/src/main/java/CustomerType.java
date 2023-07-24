964249410efdac35e1537edb679b8000efaaa0f5
Cédric PONTET <cpontet@agilepartner.net>
2023-07-24 14:28:43 +0200

    cleanup of CustomerType

--- 08_Calculate_Refactoring/02_solution/src/main/java/CustomerType.java
+++ 08_Calculate_Refactoring/02_solution/src/main/java/CustomerType.java
@@ -1,45 +1,32 @@
 public enum CustomerType {
-    NOT_REGISTERED (1) {
+    NOT_REGISTERED (1, 0.0) {
         @Override
         public double discount(int years) {
             return 0;
         }
-
     },
-    SIMPLE (2) {
-        @Override
-        public double ratio() {
-            return 0.1;
-        }
-    },
-    VALUABLE (3) {
-        @Override
-        public double ratio() {
-            return 0.3;
-        }
-    },
-    MOST_VALUABLE (4) {
-        @Override
-        public double ratio() {
-            return 0.5;
-        }
-    };
+    SIMPLE (2, 0.1),
+    VALUABLE (3, 0.3),
+    MOST_VALUABLE (4, 0.5);
 
     private final int value;
+    private final double ratio;
 
-    CustomerType (int value) {
+    CustomerType (int value, double ratio) {
         this.value = value;
+        this.ratio = ratio;
     }
 
     public int value() {
         return value;
     }
 
+    public double ratio() {
+        return ratio;
+    }
+
     public double discount(int years) {
         return (years > 5) ? (double) 5 / 100 : (double) years / 100;
     }
 
-    public double ratio() {
-        return 0.0;
-    }
 }
