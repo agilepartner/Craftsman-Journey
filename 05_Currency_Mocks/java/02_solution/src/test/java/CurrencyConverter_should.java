72e2177d61c6a204fd55033d916a082966f6d7bb
Cédric PONTET <cpontet@agilepartner.net>
2024-01-25 10:58:35 +0100

    fix typos in tests

--- 05_Currency_Mocks/java/02_solution/src/test/java/CurrencyConverter_should.java
+++ 05_Currency_Mocks/java/02_solution/src/test/java/CurrencyConverter_should.java
@@ -40,7 +40,7 @@ public class CurrencyConverter_should {
     }
 
     @Test
-    void fails_when_exchange_rate_is_not_found() {
+    void fail_when_exchange_rate_is_not_found() {
         // Arrange
         when(rates.getRate(Currency.USD, Currency.EUR))
                 .thenThrow(new ExchangeRateNotFoundException(Currency.USD, Currency.EUR, "Rate not found"));
@@ -50,42 +50,42 @@ public class CurrencyConverter_should {
     }
 
     @Test
-    void fails_when_from_currency_is_null() {
+    void fail_when_from_currency_is_null() {
         assertThrows(IllegalArgumentException.class,
                 () -> converter.convert(null, Currency.EUR, 100.0));
     }
 
     @Test
-    void fails_when_from_currency_is_empty() {
+    void fail_when_from_currency_is_empty() {
         assertThrows(IllegalArgumentException.class,
                 () -> converter.convert("", Currency.EUR, 100.0));
     }
 
     @Test
-    void fails_when_to_currency_is_null() {
+    void fail_when_to_currency_is_null() {
         assertThrows(IllegalArgumentException.class,
                 () -> converter.convert(Currency.USD, null, 100.0));
     }
 
     @Test
-    void fails_when_to_currency_is_empty() {
+    void fail_when_to_currency_is_empty() {
         assertThrows(IllegalArgumentException.class,
                 () -> converter.convert(Currency.USD, "",100.0));
     }
 
     @Test
-    void fails_when_amount_is_negative() {
+    void fail_when_amount_is_negative() {
         assertThrows(IllegalArgumentException.class,
                 () -> converter.convert(Currency.USD, Currency.EUR,-20.0));
     }
 
     @Test
-    void fails_when_amount_is_zero() {
+    void fail_when_amount_is_zero() {
         assertThrows(IllegalArgumentException.class,
                 () -> converter.convert(Currency.USD, Currency.EUR,0.0));
     }
     @Test
-    void fails_when_converted_amount_is_more_than_double_max_value() {
+    void fail_when_converted_amount_is_more_than_double_max_value() {
         when(rates.getRate(Currency.USD, Currency.EUR)).thenReturn(10.0);
 
         assertThrows(IllegalArgumentException.class,
